f2d6f66b08e65e072cfaab3f9009b090dfe7ce48
Phong Trinh <101618268@student.swin.edu.au>
2017-05-03 12:31:31 +1000

    text file creator class added

---
+++ OpenProject/TextFileCreator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace OpenProject
+{
+	public enum Mode
+	{
+		Day,
+		EarlyBird,
+		NightOwl,
+	}
+
+	public class TextFileCreator
+	{
+		public Mode _currentMode = Mode.Day;
+
+		int time = 0;
+
+		public void Create(string filename)
+		{
+			StreamWriter writer = new StreamWriter(filename);
+			for (int i = 0; i < 16; i++)
+			{
+				string str = string.Format("{0} {1}", time.ToString("D4"), Availability.No);
+				writer.WriteLine(str);
+				time = time + 30;
+				if (time == 2400)
+					time = 0;
+			}
+		}
+
+		//if (_currentMode == Mode.Day)
+		//{
+		//					time = 1300;
+		//		Create("day.txt");
+		//	break;
+		//		case "EarlyBird":
+		//		time = 500;
+		//		Create("earlybird.txt");
+		//	break;
+		//		case "NightOwl":
+		//		time = 2100:
+		//		Create("nightowl.txt");
+		//	break;
+		//}
+		//}
+
+
+		public TextFileCreator()
+		{
+		}
+	}
+}
