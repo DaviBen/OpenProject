71d99e8858c9c27cdc9475d80b87e28884caed53
BENJAMIN <100614799@student.swin.edu.au>
2017-05-06 12:25:56 +1000

    created block and timetable classses

--- OpenProject/Block.cs
+++ OpenProject/Block.cs
@@ -3,8 +3,16 @@ namespace OpenProject
 {
 	public class Block
 	{
+		private Availability _availability;
+
 		public Block()
 		{
+		}
+
+		public Availability Availability
+		{
+			get { return _availability; }
+			set { _availability = value; }
 		}
 	}
 }
--- OpenProject/TimeTable.cs
+++ OpenProject/TimeTable.cs
@@ -3,8 +3,28 @@ namespace OpenProject
 {
 	public class TimeTable
 	{
+		private Block[,] _times;
+
 		public TimeTable()
 		{
+			// Rows:    24 hours broken up into 1/2 hour blocks
+			// Columns: Monday-Friday
+			_times = new Block[48, 5];
+			_times.Initialize();
 		}
+
+		// Changes the specified block to the specified availability
+		public void ChangeBlock(int hour, int day, Availability toChange)
+		{
+			_times[hour, day].Availability = toChange;
+		}
+
+		// Returns the availability of the specified block
+		public Availability CheckBlock(int hour, int day)
+		{
+			return _times[hour, day].Availability;	
+		}
+
+
 	}
 }
